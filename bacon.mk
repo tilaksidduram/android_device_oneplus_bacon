8579cb36a7dc389dc428c2c9c5b195724ff7d86d
tilaksidduram <tilaksidduram@gmail.com>
2015-10-22 12:28:44 +0530

    bacon: Build the BoringSSL compatibility wrapper
    *this commit fixes the crash observed with the camera app.

--- bacon.mk
+++ bacon.mk
@@ -96,6 +96,10 @@ PRODUCT_PROPERTY_OVERRIDES +=
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+# BoringSSL compatability wrapper
+PRODUCT_PACKAGES += \
+    libboringssl-compat
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.bacon
