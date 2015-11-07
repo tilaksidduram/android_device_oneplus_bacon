beed2710556e80871b4ce0974bab060711f03f13
tilaksidduram <tilaksidduram@gmail.com>
2015-11-07 10:04:24 +0530

    Revert "bacon: Build the BoringSSL compatibility wrapper"
    This reverts commit 8579cb36a7dc389dc428c2c9c5b195724ff7d86d.

--- bacon.mk
+++ bacon.mk
@@ -95,10 +95,6 @@ PRODUCT_PROPERTY_OVERRIDES +=
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# BoringSSL compatability wrapper
-PRODUCT_PACKAGES += \
-    libboringssl-compat
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.bacon
