06ca6d0a856c37be03f029c5e7765823caf0a28c
tilaksidduram <tilaksidduram@gmail.com>
2017-07-20 22:15:00 +0530

    Bacon: Enable real time lockscreen charging info

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,5 +190,8 @@ WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Inherit from the proprietary version
 include vendor/oneplus/bacon/BoardConfigVendor.mk
